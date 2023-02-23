d3767e315147bb1fbdaf1bd6ced06df497ebea33
hyeding <g31373947@gmail.com>
2023-02-23 17:57:58 +0900

    feat: add front-end code and modify build.gradle. add deploy.yml

---
+++ src/test/java/mybucketlistbook/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package mybucketlistbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
